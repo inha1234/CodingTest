6feeca630fae1cc97ae487bcdbbe6539e2562f38
inha1234 <118185571+inha1234@users.noreply.github.com>
2023-03-13 13:58:59 +0900

    [level 0] Title: k의 개수, Time: 1.86 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        int tmp = 0;
+        for(int num = i; num<=j; num++){
+            tmp = num;
+            while(tmp>0){
+                if(tmp%10==k){
+                    answer++;
+                }
+                tmp /=10;
+            }
+        }
+        return answer;
+    }
+}
