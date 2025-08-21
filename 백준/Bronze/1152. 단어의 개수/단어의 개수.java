c48db30d9605803d378ab0e36db862704106d0ab
inha1234 <118185571+inha1234@users.noreply.github.com>
2025-08-21 21:47:28 +0900

    [Bronze II] Title: 단어의 개수, Time: 484 ms, Memory: 63648 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        sc.close();
+        
+
+        str = str.trim();
+        
+        if (str.isEmpty()) {
+            System.out.println(0);
+        } else {
+            String[] words = str.split(" ");
+            System.out.println(words.length);
+        }
+    }
+}
